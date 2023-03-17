5562a475e5f5f6f52228a1215a7cd4e907f14eed
wallacebrf <62187108+wallacebrf@users.noreply.github.com>
2023-03-17 10:55:24 -0500

    Update temp_data_logger.ino
    added ability to contact healthchecks.io to confirm arduino is still online
    removed static IP address and replaced with DHCP
    updated to use latest libraries
    uses DNS to look up the healthchecks.io ping domain

--- temp_data_logger.ino
+++ temp_data_logger.ino
@@ -1,26 +1,27 @@
 #include <avr/io.h>
 #include <avr/pgmspace.h>
-//#include <Ethernet2.h>//needed for newer 5500 Ethernet shield chip sets
 #include <Ethernet.h>//needed for older 5200 Ethernet shield chip sets 
 #include <OneWire.h>
-#include <avr/pgmspace.h>
 #include <avr/wdt.h>
+#include <Dns.h>
 
 //USER DEFINED VARIABLES
-byte mac[] = {0x00, 0xAA, 0xBB, 0xCC, 0xDE, 0x01 }; //MAC address assigned to the Arduino Ethernet Shield
-byte localip[] = {192, 168, 1, 45}; //STATIC IP address assigned to the Arduino Ethernet Shield
-byte serverip[] = {192, 168, 1, 13}; //target server IP address running the PHP script
-#define CSG1wirepin 38 //arduino PIN used by the 1-wire temp sensor #1
-#define MSG1wirepin 44 //arduino PIN used by the 1-wire temp sensor #2
-long interval = 60000; // READING INTERVAL --> how often is a data sample taken, this default value = every 60 seconds
+IPAddress subnet(255,255,255,0);
+IPAddress healthcheckio_IP;
+byte mac[] = {0x00, 0xAA, 0xBB, 0xCC, 0xDE, 0x05 };       //MAC address assigned to the Arduino Ethernet Shield
+byte serverip[] = {192, 168, 1, 13};                      //target server IP address running the PHP script to ingest temperature data
+#define CSG1wirepin 22                                    //arduino PIN used by the 1-wire temp sensor #1
+#define MSG1wirepin 23                                    //arduino PIN used by the 1-wire temp sensor #2
+long interval_temp = 60000;                               // READING INTERVAL --> how often is a data sample taken, this default value = every 60 seconds
+long interval_heartbeat = 900000;                         // READING INTERVAL --> how often is healthchecks.io contacted for a heartbeat update? value = 15 minutes 
 byte debug=0;
+const char* ip_to_str(const uint8_t*);
 
 EthernetClient client;
+DNSClient dnClient;
 
-
-
-
-long previousMillis = 0;
+long previousMillis_temp = 0;
+long previousMillis_heartbeat = 0;
 unsigned long currentMillis = 0;
 byte middle_temp_whole;                            
 byte cold_side_temp_whole; 
@@ -32,15 +33,83 @@ int cold_badsensorcount =0;
 int middle_badsensorcount =0;
 byte temp_scale = 1;
 
+// Just a utility function to nicely format an IP address.
+const char* ip_to_str(const uint8_t* ipAddr)
+{
+  static char buf[16];
+  sprintf(buf, "%d.%d.%d.%d\0", ipAddr[0], ipAddr[1], ipAddr[2], ipAddr[3]);
+  return buf;
+}
+
 void setup() { 
   Serial.begin(115200);
+  //Ethernet.begin(mac, ip, myDNS, gateway,subnet); 
+  
+  Serial.println(F("Version 3.0 3/8/2023. booting....."));
+  Serial.println(F("Initialize Ethernet with DHCP:"));
 
-  Ethernet.begin(mac, localip);
-  Serial.print(F("Version 2.0 1/28/2022. booting....."));
+  if (Ethernet.begin(mac) == 0) {
+    Serial.println(F("Failed to configure Ethernet using DHCP"));
+    if (Ethernet.hardwareStatus() == EthernetNoHardware) {
+      Serial.println(F("Ethernet shield was not found.  Sorry, can't run without hardware. :("));
+    } else if (Ethernet.linkStatus() == LinkOFF) {
+      Serial.println(F("Ethernet cable is not connected."));
+    }
+
+    // no point in carrying on, so do nothing forevermore:
+
+    while (true) {
+      delay(1);
+    }
+
+  }
+
+  // print your local IP address:
+  Serial.print("My IP address: ");
+  Serial.println(Ethernet.localIP());
+  
+  dnClient.begin(Ethernet.dnsServerIP());
+  if(dnClient.getHostByName("hc-ping.com",healthcheckio_IP) == 1) {
+    Serial.print(F("hc-ping.com = "));
+    Serial.println(healthcheckio_IP);
+    Serial.println("");
+    Serial.println("");
+  }else{ 
+    Serial.print(F("dns lookup failed"));
+  }
+ 
   wdt_enable(WDTO_4S);
 }
 
 void loop(){
+  //maintain DHCP IP lease
+  switch (Ethernet.maintain()) {
+    case 1:
+      //renewed fail
+      Serial.println(F("Error: renewed fail"));
+      break;
+    case 2:
+      //renewed success
+      Serial.println(F("Renewed success"));
+      //print your local IP address:
+      Serial.print(F("My IP address: "));
+      Serial.println(Ethernet.localIP());
+      break;
+    case 3:
+      //rebind fail
+      Serial.println(F("Error: rebind fail"));
+      break;
+    case 4:
+      //rebind success
+      Serial.println(F("Rebind success"));
+      //print your local IP address:
+      Serial.print(F("My IP address: "));
+      Serial.println(Ethernet.localIP());
+      break;
+    default:
+      //nothing happened
+      break;
+  }
      wdt_reset();
      byte temp_whole, temp_fract, temp_status;
      currentMillis = millis();
@@ -48,26 +117,26 @@ void loop(){
      middle_temp_whole = temp_whole;
      middle_temp_fract = temp_fract;
      middle_temp_status = temp_status;
-     Serial.print(temp_whole);
-     Serial.print(".");
-     Serial.println(temp_fract);
+     //Serial.print(temp_whole);
+     //Serial.print(".");
+    // Serial.println(temp_fract);
      CONVERT_TEMP(CSG1wirepin, temp_whole, temp_fract, temp_status);
      cold_side_temp_whole = temp_whole;
      cold_side_temp_fract = temp_fract;
      cold_side_temp_status = temp_status;
-     Serial.print(temp_whole);
-     Serial.print(".");
-     Serial.println(temp_fract);
+     //Serial.print(temp_whole);
+     //Serial.print(".");
+     //Serial.println(temp_fract);
      
-  if(currentMillis - previousMillis > interval) { // READ ONLY ONCE PER INTERVAL
-      previousMillis = currentMillis; 
+  if(currentMillis - previousMillis_temp > interval_temp) { // READ ONLY ONCE PER INTERVAL
+      previousMillis_temp = currentMillis; 
       if(debug==1){
-        Serial.print(F("Times Up"));
+        Serial.print(F("Temperature Times Up"));
       }
 
-      if (client.connect(serverip,80)) { // REPLACE WITH YOUR SERVER ADDRESS
-        Serial.println(F("Client Connected"));
-        client.print(F("GET /admin/temperature_add.php?"));
+      if (client.connect(serverip,80)) { 
+        Serial.println(F("Temperature Client Connected"));
+        client.print(F("GET /admin/equipment_cabinet_add.php?"));
         client.print(F("middle_temp_whole="));
         client.print(middle_temp_whole); 
         client.print(F("&middle_temp_fract="));
@@ -100,7 +169,7 @@ void loop(){
         client.stop();
         client.stop();
       } else{
-          Serial.print(F("could not connect to server"));
+          Serial.print(F("Temperature Logging could not connect to server"));
       }
 
       if(debug==1){
@@ -124,6 +193,31 @@ void loop(){
         Serial.println(); 
       }
     }
+
+if(currentMillis - previousMillis_heartbeat > interval_heartbeat) { // PERFORM ONLY ONCE PER INTERVAL
+      previousMillis_heartbeat = currentMillis; 
+      if(dnClient.getHostByName("hc-ping.com",healthcheckio_IP) == 1) {
+		Serial.print(F("hc-ping.com = "));
+		Serial.println(healthcheckio_IP);
+	  }else{
+		Serial.print(F("dns lookup failed"));
+	  }
+      if(debug==1){
+        Serial.println(F("Heartbeat Times Up"));
+      }
+
+      if (client.connect(healthcheckio_IP,80)) {
+        Serial.println(F("Heartbeat Client Connected"));
+        Serial.println("");
+        client.println(F("GET /xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx HTTP/1.1"));
+        client.println(F("Host: hc-ping.com"));
+        client.println(F("Connection: close"));
+        client.println();
+        client.stop();
+      } else{
+          Serial.print(F("Heartbeat could not connect to server"));
+      }
+  }
 }
 
 
@@ -206,12 +300,4 @@ void CONVERT_TEMP(byte Sensor_PIN, byte & temp_whole, byte & temp_fract, byte & 
        temp_status = 0;//the sensor must be bad as the temperature must never get down as low as 0 degrees C
      }
    }
-}
-
-// Just a utility function to nicely format an IP address.
-const char* ip_to_str(const uint8_t* ipAddr)
-{
-  static char buf[16];
-  sprintf(buf, "%d.%d.%d.%d\0", ipAddr[0], ipAddr[1], ipAddr[2], ipAddr[3]);
-  return buf;
 }
